ff34cf725cee275b9e5852304c8ac64f752693a6
acil <110228112+acilll45@users.noreply.github.com>
2022-08-21 22:29:52 -0700

    Add files via upload

--- hermanChnl.js
+++ hermanChnl.js
@@ -2287,6 +2287,52 @@ ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Obje
                 hermanChnl.sendContact(m.chat, global.owner, m)
             }
             break
+            case 'rules': case 'peraturan':
+            m.reply('––––––◸ *RULES* ◿––––––
+
+_Kebijakan privasi atau Private without being in public_
+
+
+
+• *Kebijakan Privasi:*
+1. WhatsApp Bot tidak akan merekam data riwayat chat user.
+2. WhatsApp Bot tidak akan menyebarkan nomor users.
+3. WhatsApp Bot tidak akan menyimpan media yang dikirimkan oleh users.
+4. WhatsApp Bot tidak akan menyalah gunakan data data users.
+5. Owner WhatsApp Bot berhak melihat data riwayat chat users.
+6. Owner WhatsApp Bot berhak melihat status users.
+7. Owner WhatsApp Bot dapat melihat riwayat chat, dan media yang dikirimkan users.
+
+• Jika ada bug/eror di website kami saya mohon untuk Report nya, tanpa biaya dan aman
+
+_Cara penggunaan Acil BOT Agar terhindar dari Suspand_
+
+• *Peraturan WhatsApp Bot:*
+1. Users dilarang menelpon maupun memvideo call nomor bot.
+2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
+3. Users diharap tidak melakukan spam dalam penggunaan bot.
+4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi Owner.
+5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
+
+• *Note:*
+1. Jika ada yang menjual/beli/sewa bot atas nomor ini, harap segera hubungi owner!
+2. Jika ingin donasi bisa langsung aja ya social payment Dana 
+3. Ketik .sewa Jika Ingin SewaBot 
+
+•Agar terhindar dari Suspand/Ban kalian bisa membaca juga di Peraturan kami.
+
+•Perlu kalian tahu bahwa kami menjaga Privasi dari data-data anda!
+
+• *Syarat Ketentuan WhatsApp Bot:*
+
+1. WhatsApp Bot akan keluar dari group jika ada salah satu member melanggar peraturan.
+2. WhatsApp Bot dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. WhatsApp Bot tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.
+4. WhatsApp Bot akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. WhatsApp Bot bertanggung jawab atas kesalahan fatal dalam programing maupun owner.'
+}
+break
+
             case 'list': case 'menu': case 'help': case '?': {
                 anu = `SELAMAT MENIKMATI
 
@@ -2786,8 +2832,8 @@ ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Obje
                                 }  
                             }, {
                                 quickReplyButton: {
-                                    displayText: '❝𝐒𝐂𝐑𝐈𝐏𝐓❞',
-                                    id: 'sc'
+                                    displayText: '❝𝐑𝐔𝐋𝐄𝐒 𝐁𝐎𝐓❞',
+                                    id: 'rules'
                                 }
                             }]
                         }
